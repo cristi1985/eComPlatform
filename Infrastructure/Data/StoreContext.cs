c037a3a67df3d41ac815db0dcd7f05f44a8f8f17
Cristi Simion <cristian@interactiveschools.com>
2020-10-23 10:08:09 +0300

    Add config for migrations

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
@@ -14,5 +15,16 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        //Suprascriem metoda OnModelCreating din DbContext care este responsabila cu 
+        //creeare migrarii astfel incat sa chemam configurarea facuta de noi in ProductConfiguration
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
