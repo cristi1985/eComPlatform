8cc9b41c23dee63db07fe4cdec9345e39994671d
Cristi Simion <cristian@interactiveschools.com>
2020-11-19 14:36:07 +0200

    Get product name on home page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'eComPlatform';
+  products: IProduct[]=[];
+  //response: IPagination[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response:IPagination) => {
+      this.products = response.data!;
+      console.log(this.products);
+    }, error => { console.log(error)})
+  }
 }
