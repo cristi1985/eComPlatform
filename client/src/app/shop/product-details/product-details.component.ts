d8817bdc37e2390490c2553001734bc4d0774e98
Cristi Simion <cristian@interactiveschools.com>
2020-12-14 21:06:37 +0200

    Titles in breadcrumbs and timer

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../../shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,15 +12,18 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product!: IProduct;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService:BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
   }
 
   loadProduct() {
-    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('{id}')!).subscribe(product => {
-      this.product = product;
+    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')!).subscribe(prod => {
+      //console.log('Product is', product);
+      this.product = prod;
+      console.log('Product is', this.product)
+      this.bcService.set('@productDetails', this.product.productName!)
     }, error => {
         console.log(error);
     })
