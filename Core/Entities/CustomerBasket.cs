e700a41bd1a704129da576565cb4c8b1c0b38be8
Cristi Simion <cristian@interactiveschools.com>
2020-12-19 10:28:55 +0200

    Add basket API using Redis

---
+++ Core/Entities/CustomerBasket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class CustomerBasket
+    {
+        public CustomerBasket(string id)
+        {
+            Id = id;
+        }
+
+        public CustomerBasket() { }
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
