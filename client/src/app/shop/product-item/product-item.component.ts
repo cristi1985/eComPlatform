d62fa59800c77c7d53bcb8b0782a159b03dc7f09
Cristi Simion <cristian@interactiveschools.com>
2020-12-20 21:18:41 +0200

    Add basket functionality to client side

--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IProduct } from '../../shared/models/product';
+import { BasketService } from '../../basket/basket.service';
 
 
 @Component({
@@ -11,9 +12,13 @@ export class ProductItemComponent implements OnInit {
   @Input()
     product!: IProduct;
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product);
   }
 
 }
