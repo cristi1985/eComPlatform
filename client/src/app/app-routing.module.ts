6ea1fb7fcf441fbed0d7e67227e4297c7504138a
Cristi Simion <cristian@interactiveschools.com>
2021-01-03 20:13:31 +0200

    Checkout on front-end

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found' } },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'Magazin' } },
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: { breadcrumb: 'Cos' } },
-  
+  { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule), data: { breadcrumb: 'Plata' } },
   { path: '**', redirectTo: 'not-found', pathMatch:'full' }];
 
 @NgModule({
