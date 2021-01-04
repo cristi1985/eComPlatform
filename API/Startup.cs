8d5aa489838506e1c52a067fc168da8f38280e3a
Cristi Simion <cristian@interactiveschools.com>
2021-01-04 19:42:14 +0200

    Identity

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,10 @@ namespace API
             services.AddControllers();
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppIdentityDbContext>(x =>
+            {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            });
             services.AddSingleton<IConnectionMultiplexer>(c =>
             {
                 var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
