13b989b003f4f3c8c197b49740538a6249f26847
Cristi Simion <cristian@interactiveschools.com>
2021-01-11 09:41:24 +0200

    Validation errors when adding user account

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -96,6 +96,10 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email address is in use" }});
+            }
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
@@ -115,6 +119,9 @@ namespace API.Controllers
             };
         }
 
-       
+        private ActionResult<UserDto> BadRequestObjectResult(ApiValidationErrorResponse apiValidationErrorResponse)
+        {
+            return BadRequest(apiValidationErrorResponse);
+        }
     }
 }
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,15 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", ErrorMessage ="Password must have 1 Uppercase, 1 Lowerase, 1 number, 1 alphanumeric character and at least 6 characters")]
         public string Password { get; set; }
     }
 }
