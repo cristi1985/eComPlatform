2f5c7a92b24838213d770efb2719c8b06caa9332
Cristi Simion <cristian@interactiveschools.com>
2021-02-10 10:27:23 +0200

    Added error messages on form

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AsyncValidator, AsyncValidatorFn, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { of, timer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { AccountService } from '../account.service';
+
 
 @Component({
   selector: 'app-register',
@@ -10,7 +13,7 @@ import { AccountService } from '../account.service';
 })
 export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
-
+  errors!: string[];
   constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
@@ -20,7 +23,9 @@ export class RegisterComponent implements OnInit {
   createRegisterForm() {
     this.registerForm = this.fb.group({
       displayName: [null, [Validators.required]],
-      email: [null, [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
+      email: [null,
+        [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')],
+        [this.validateEmailNotTaken()]],
       password: [null, [Validators.required]]  })
   }
 
@@ -29,7 +34,25 @@ export class RegisterComponent implements OnInit {
       this.router.navigateByUrl('/shop');
     }, error => {
         console.log(error);
+        this.errors = error.errors;
     })
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return control => {
+      return timer(500).pipe(
+        switchMap(() => {
+          if (!control.value) {
+            return of(null);
+          }
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map(res => {
+              return res ? { emailExists: true } : null;
+            })
+          );
+        })
+      );
+    };
   }
 
 }
