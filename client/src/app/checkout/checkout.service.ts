ef1ddc7b2d9c8809eedec639ad50cfb0886ff133
Cristi Simion <cristian@interactiveschools.com>
2021-02-18 00:06:58 +0200

    add jwt token through interceptor and form fields

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { IDeliveryMethod } from '../shared/models/deliveryMethods';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getDeliveryMethods() {
+    return this.http.get(this.baseUrl + 'orders/deliveryMethods').pipe(
+      map((dm: IDeliveryMethod[]) => {
+        return dm.sort((a, b) => b.price - a.price);
+      })
+  )};
 }
